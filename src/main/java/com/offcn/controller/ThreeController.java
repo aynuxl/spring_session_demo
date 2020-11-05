65437d06250293d82c5d3f1c332a6c28a07909ec
aynuxl <871752024@qq.com>
2020-11-05 20:01:17 +0800

    update Three

--- src/main/java/com/offcn/controller/ThreeController.java
+++ src/main/java/com/offcn/controller/ThreeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class ThreeController {
     @GetMapping("/three")
     public String thre(){
-
+        System.out.println("333333333333");
         return "ok";
     }
 }
